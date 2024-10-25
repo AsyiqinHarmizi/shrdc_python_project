f928e25af02755acbec8a1e1e29953647f1ffc0d
Nurul Asyiqin Mohd Harmizi <61441133+AsyiqinHarmizi@users.noreply.github.com>
2024-10-25 18:39:17 +0800

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -10,7 +10,7 @@ st.title("COVID-19 in Mexico: A Comprehensive Analysis of Patient Outcomes in 20
 st.info("Overview of the webpage")
 st.write("""Welcome to Our COVID-19 Analysis of Patient Outcomes in Mexico for 2020!
 
-In this webpage, we present an in-depth analysis of COVID-19 patient outcomes in Mexico during 2020. Dive into our comprehensive dataset to uncover insights and trends that shaped the healthcare landscape during this challenging year.
+In this webpage, we present an in-depth analysis of COVID-19 patient outcomes in Mexico during year 2020. Dive into our comprehensive dataset to uncover insights and trends that shaped the healthcare landscape during this challenging year.
 
 Explore our interactive charts and detailed analyses, which highlight key findings and provide a clearer understanding of the impact of the pandemic on Mexican patients.
 
